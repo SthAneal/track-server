926c78f9f0b0bb3ed2ee94c80f1cd9d8df7a29f1
sthAneal <anilmeronaam@gmail.com>
2021-04-05 22:58:51 +1000

    =

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     MONGO_URI: process.env.MONGO_URI,
-    MY_SECURE_KEY: process.env.MY_SECURE_KEY
+    MY_SECRET_KEY: process.env.MY_SECURE_KEY
 };
